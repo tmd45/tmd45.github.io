bee766e6e0c72914f319bcb6dd174ad3df8f5499
Yoko TAMADA <tamada@feedforce.jp>
2014-02-18 10:34:36 +0900

    静的ファイルのディレクトリ名を変更 ついでにindex.htmlからのリンク先をmiddlemanの日本語ページにした

--- config.rb
+++ config.rb
@@ -45,22 +45,20 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
+set :css_dir, 'css'
+set :js_dir, 'js'
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
