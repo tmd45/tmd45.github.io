027bbbfa52f233426b458ebf0f50daadaa861478
tmd45 <tmd45@otegami.jpn.org>
2014-03-20 01:56:36 +0900

    bootstrap.jsが読み込めていなかったのでall.jsでinclude

--- source/js/all.js
+++ source/js/all.js
@@ -1,1 +1,32 @@
+//= require bootstrap
 //= require_tree .
+
+$(document).ready(function() {
+  var heroImage = $('header .hero-image');
+  if(heroImage.length > 0) {
+    $(window).scroll(function() {
+      var $this = $(this);
+      var top = $this.scrollTop();
+      heroImage.css('transform', 'translateY(' + (top/2) + 'px)');
+    });
+  }
+
+  $('.carousel').carousel({ interval: 0 });
+  $('#activity-carousel').load(function() {
+    $(this).carousel('pause');
+  });
+});
+
+$(window).load(function() {
+  // Hero image + navbar
+  var $navbar = $('.navbar-inner');
+  $navbar.waypoint('sticky', {
+    wrapper: '<span class="sticky-wrapper" />',
+    stuckClass: 'navbar-fixed-top',
+    direction: 'down right',
+    target: '.navbar'
+  });
+
+// $navbar.parent().waypoint('sticky', { stuckClass: 'navbar-fixed-bottom', offset: 'bottom-in-view', direction: 'up', target: '.home' });
+// if($navbar.is(':below-the-fold')) $navbar.toggleClass('navbar-fixed-bottom', true); 
+});
