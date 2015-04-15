df6cba58ac664816cda6fc5ad2957dcc2905dc6f
Yoko TAMADA <tamada@feedforce.jp>
2015-04-15 22:00:39 +0900

    [wip] Designed index

--- source/js/init.js
+++ source/js/init.js
@@ -1,7 +1,13 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav()
+    $('.button-collapse').sideNav({
+      closeOnClick: true
+    })
+
+    $('.collapsible').collapsible();
+
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
