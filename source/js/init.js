add546f0d98e42766dc754efdefd444661cc72f5
tmd45 <tmd45@otegami.jpn.org>
2016-04-03 22:55:24 +0900

    Fix init.js
    Uncaught TypeError: Cannot read property 'bottom' of undefined

--- source/js/init.js
+++ source/js/init.js
@@ -7,7 +7,5 @@
 
     $('.scrollspy').scrollSpy();
 
-    $('.powered-wrapper').pushpin({ top: $('.powered-wrapper').offset().bottom });
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
