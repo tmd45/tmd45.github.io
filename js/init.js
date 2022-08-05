1ab3ab7e83161527e94ab78137aa5cc6a53c8ca6
circleci <tmd45@otegami.jpn.org>
2022-08-05 18:31:47 +0900

    Automated commit at 2022-08-05 09:31:47 UTC by middleman-deploy 2.0.0-alpha

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('.collapsible').collapsible();
+
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
