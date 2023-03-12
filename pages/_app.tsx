320935b5e9a9293da160c5dd71118c1e927a65c0
tmd45 <tmd45@otegami.jpn.org>
2023-03-13 01:50:26 +0900

    Add HeadLinkFavicons in Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import * as React from "react"
 import Head from "next/head"
 import { AppProps } from "next/app"
+
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import { CacheProvider, EmotionCache } from "@emotion/react"
 import theme from "../src/theme"
 import createEmotionCache from "../src/createEmotionCache"
+
+import HeadLinkFavicons from "../components/HeadLinkFavicons"
 import { Layout } from "../components/Layout"
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -22,6 +25,7 @@ export default function MyApp(props: MyAppProps) {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <HeadLinkFavicons />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
