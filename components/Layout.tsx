a56a0d298607bcf13b98ff1dce14041c23225ddf
tmd45 <tmd45@otegami.jpn.org>
2023-03-20 22:48:30 +0900

    Add Head and title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Container, Stack, Box, useTheme } from "@mui/material"
 import SideMenu from "../components/SideMenu"
+import Head from "next/head"
 
 interface Props {
   readonly children: React.ReactNode
@@ -10,25 +11,31 @@ export const Layout: React.FC<Props> = ({ children }) => {
   const theme = useTheme()
 
   return (
-    <Container
-      maxWidth="xl"
-      sx={{
-        borderTop: `5px solid ${theme.palette.primary.dark}`,
-      }}
-    >
-      <Stack direction="row">
-        <SideMenu />
-        <Box
-          sx={{
-            width: "100%",
-            padding: "32px",
-          }}
-        >
-          <Container maxWidth="md" disableGutters>
-            {children}
-          </Container>
-        </Box>
-      </Stack>
-    </Container>
+    <>
+      <Head>
+        <title>tmd45.jp</title>
+        <meta property="og:title" content="tmd45.jp" key="title" />
+      </Head>
+      <Container
+        maxWidth="xl"
+        sx={{
+          borderTop: `5px solid ${theme.palette.primary.dark}`,
+        }}
+      >
+        <Stack direction="row">
+          <SideMenu />
+          <Box
+            sx={{
+              width: "100%",
+              padding: "32px",
+            }}
+          >
+            <Container maxWidth="md" disableGutters>
+              {children}
+            </Container>
+          </Box>
+        </Stack>
+      </Container>
+    </>
   )
 }
